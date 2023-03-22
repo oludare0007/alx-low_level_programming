719ad10c11c397f402c2e108cbd77c1d074cda52
oludare0007 <oludareayodele@gmail.com>
2023-03-22 09:27:41 -0700

    main.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
-#ifndef _HEADER_
+#ifndef _HEADER_i
 #define _HEADER_
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
 #endif
 

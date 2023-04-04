69b579e8a338f3b2af3b17557cf91810af54b452
oludare0007 <oludareayodele@gmail.com>
2023-04-04 05:35:31 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,9 +13,9 @@ size_t print_listint(const listint_t *h)
 /* while loop area */
 	while (h)
 	{
-		printf("%d\n", h->n);
-		num++;
+		printf("%d\n", h->n)
 		h = h->next;
+		num++;
 	}
 	return (num);
 /* exit code */

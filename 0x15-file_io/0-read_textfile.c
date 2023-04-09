f7da76792198d671af31bfef2f1fd590db642d8e
oludare0007 <oludareayodele@gmail.com>
2023-04-09 06:21:41 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 /**
  *  reads a text file and prints it to the POSIX standard output
  *
- *  @filename: 
+ *  @filename: filename
  *  @letters: number of letters printed is seen.
  *
  *  Return: if failed, return 0.
@@ -19,7 +19,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
+
 
 	if (fd == -1)
 		return (0);
@@ -27,9 +28,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!btt)
 		return (0);
 	ndr = read(fd, btt, letters);
-	nr = write(STDOUT_FILRNO, btt, nr);
+	nr = write(STDOUT_FILENO, btt, ndr);
 
-	clode(fd);
+	close(fd);
 
 	free(btt);
 

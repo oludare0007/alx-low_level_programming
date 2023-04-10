aad35638717a1b1329d67e97f89d273cddbbfcaa
oludare0007 <oludareayodele@gmail.com>
2023-04-10 08:49:08 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*
+* set_bit - set value bit to 1
+* @n: input
+* @index: input
+*
+* Return: bits at index if success(1) or error (-1)
+*
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n = *n | (1 << index);
+	return (1);
+}
